9fed69b15540e3a514c4f5d3c2dfdf977bb2f35a
aliciarolineko <alicia.neko@mail.ru>
2024-09-17 07:07:45 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -27,6 +27,7 @@ print("El bot se ha iniciado, para detenerlo pulse CTRL+C")
 
 # Define the admin and temporary users
 admin_users = list(map(int, os.getenv('ADMINS').split(',')))
+users = list(map(int, os.getenv('USERS').split(',')))
 temp_users = []
 
 # Combine admin_users and temp_users into allowed_users
