eee9b03a652d297e01bb939300ba116d29bef885
Naki <alicia.neko@mail.ru>
2024-12-08 17:57:58 -0500

    Update moodleclient.py

--- moodleclient.py
+++ moodleclient.py
@@ -16,7 +16,7 @@ def upload_token(filename, token, host):
 	resp = s.post(f"{host}/webservice/upload.php", data=data, files=files)
 	resp = json.loads(resp.text)[0]
 	contextid, itemid, filename = resp["contextid"], resp["itemid"], resp["filename"]
-	url = f"{host}/draftfile.php/{contextid}/user/draft/{itemid}/{urllib.parse.quote(filename)}?token={token}"
+	url = f"{host}/draftfile.php/{contextid}/user/draft/{itemid}/{urllib.parse.quote(filename)}"
 	return url
 
 #print(upload_token("requirements.txt", "3e7e0d514c6ea7c7040217a37dcfcc70", "https://eva.uo.edu.cu"))
