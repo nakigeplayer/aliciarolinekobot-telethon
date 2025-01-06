7e1e9b784e4f9c9db78e00118dec00edf3e05920
Naki <alicia.neko@mail.ru>
2025-01-06 11:20:12 -0500

    Update moodleclient.py

--- moodleclient.py
+++ moodleclient.py
@@ -2,7 +2,6 @@ import requests
 import json
 import urllib
 from random import randint
-
 def upload_token(filename, token, host):
 	s = requests.session()
 	data = {
@@ -18,5 +17,3 @@ def upload_token(filename, token, host):
 	contextid, itemid, filename = resp["contextid"], resp["itemid"], resp["filename"]
 	url = f"{host}/webservice/draftfile.php/{contextid}/user/draft/{itemid}/{urllib.parse.quote(filename)}?token={token}"
 	return url
-
-#print(upload_token("requirements.txt", "3e7e0d514c6ea7c7040217a37dcfcc70", "https://eva.uo.edu.cu"))
